4efabc56bae6d409db8478589a462fc7d0b8f091
Tim F <tim.fellerhoff@gmail.com>
2022-05-17 19:47:40 +0200

    fix coreg.m

--- code/coreg.m
+++ code/coreg.m
@@ -1,31 +1,32 @@
 clear all; clc
 
-base_path = '/Volumes/Data/projects/ibis/analysis/DPARSF_test/N13';
+base_path = '/Volumes/Data/projects/ibis/analysis/DPARSF_test2/N13';
 
 subjectsDir = dir((fullfile(base_path, 'FunImgAR', 'IN*')));
 
 subjects = {subjectsDir.name};
 
-%if length(ref_folders) == length(src_folders)
-    for i = 1:length(subjects)
-        subject = char(subjects(i));
-        ref_folder_pattern = fullfile(base_path, '**', subject,'mean*.nii');
-        sub_ref = dir(ref_folder_pattern);
 
-        src_folder_pattern = fullfile(base_path, 'T1Img', subject, '/2*.nii');
-        sub_src = dir(src_folder_pattern);
-        ref = fullfile(sub_ref.folder,sub_ref.name);
-        src = fullfile(sub_src.folder,sub_src.name);
+for i = 1:length(subjects)
+    subject = char(subjects(i));
 
-        spm('defaults', 'fMRI');
-        coregistration = coreg_job(src, ref);
-        spm_jobman('run', coregistration);
+    % Move files to T1ImgCoreg as defined by DPARSF Toolbox
+    targetPath = char(fullfile(base_path, 'T1ImgCoreg', subject));
+    T1Dir = dir(fullfile(base_path, 'T1Img', subject, '*.nii'));
+    T1File = fullfile(T1Dir.folder, T1Dir.name);
+    mkdir(targetPath);
+    copyfile(T1File, targetPath);
 
-        % Move files to T1ImgCoreg as defined by DPARSF Toolbox
-        targetPath = char(fullfile(base_path, 'T1ImgCoreg', subject));
-        coregisteredDir = dir(fullfile(base_path, 'T1Img', subject, '*.nii'));
-        coregisteredFile = fullfile(coregisteredDir.folder, coregisteredDir.name);
-        mkdir(targetPath);
-        copyfile(coregisteredFile, targetPath);
-    end
-%end
+    % Estimate Coregister
+    ref_folder_pattern = fullfile(base_path, '**', subject,'mean*.nii');
+    sub_ref = dir(ref_folder_pattern);
+
+    src_folder_pattern = fullfile(base_path, 'T1ImgCoreg', subject, '/*.nii');
+    sub_src = dir(src_folder_pattern);
+    ref = fullfile(sub_ref.folder,sub_ref.name);
+    src = fullfile(sub_src.folder,sub_src.name);
+
+    spm('defaults', 'fMRI');
+    coregistration = coreg_job(src, ref);
+    spm_jobman('run', coregistration);
+end

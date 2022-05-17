726bb543a37183eb9900f5c97fc06009c880693f
Tim F <tim.fellerhoff@gmail.com>
2022-05-17 13:07:30 +0200

    added script for coreg

---
+++ code/coreg_job.m
@@ -0,0 +1,17 @@
+%-----------------------------------------------------------------------
+% Job saved on 17-May-2022 11:52:47 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+function matlabbatch = coreg(ref, source)
+    matlabbatch{1}.spm.spatial.coreg.estwrite.ref = ref;
+    matlabbatch{1}.spm.spatial.coreg.estwrite.source = source;
+    matlabbatch{1}.spm.spatial.coreg.estwrite.other = {''};
+    matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
+    matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+    matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+    matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+    matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+    matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+    matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+    matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = 'r';
